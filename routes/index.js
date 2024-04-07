4278319f9d578a1d18e807b3841c46d18a17e1a0
Sudit Kumar Jha <jsudit2@gmail.com>
2024-04-07 22:07:27 +0530

    First Commit Routes -> /, get/login, get/register EJS -> Home, Login, Register Style Sheets -> login

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// GET Login page
+
+router.get('/login', function (req, res) {
+  res.render('login')
+})
+
+// GET register page
+
+router.get('/register', function (req, res) {
+  res.render('register')
+})
 
 module.exports = router;
